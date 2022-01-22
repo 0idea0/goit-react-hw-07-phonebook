393dfd030321993642788d1b0fe94d3f2623f3b8
KYRYLO VOITOVSKYI <kirill.voitovskiy@gmail.com>
2022-01-22 15:32:55 +0200

    add hw-06

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+/* import React from 'react';
+
+import InputForm from './components/InputForm/InputForm';
+import Filter from './components/Filter/Filter';
+import Phonebook from './components/Phonebook/Phonebook.container';
+import Container from './components/Container/Container';
+
+const App = () => (
+  <>
+    <Container title="Phonebook">
+      <InputForm />
+    </Container>
+    <Container title="Contacts">
+      <Filter />
+      <Phonebook />
+    </Container>
+  </>
+);
+
+export default App;
+ */
+
 import './App.css';
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import Container from './components/Container/Container';
+import Filter from './components/Filter/Filter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container className="App">
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <h2>Contacts:</h2>
+        <Filter />
+        <ContactList />
+      </Container>
+    </>
   );
 }
 
